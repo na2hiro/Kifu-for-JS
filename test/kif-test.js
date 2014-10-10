369715911842d1cc202ef45834ca8e784736e696
na2hiro <a@81.la>
2014-10-10 13:24:30 +0900

    kifuファイル追加

--- test/kif-test.js
+++ test/kif-test.js
@@ -3,12 +3,13 @@ var fs = require('fs');
 var iconv = new (require("iconv").Iconv)("cp932", "utf-8");
 var dir = __dirname+'/files/kif/';
 var files = fs.readdirSync(dir).filter(function(file){
-	return fs.statSync(dir+file).isFile() && /.*\.kif$/.test(dir+file);
+	return fs.statSync(dir+file).isFile() && /.*\.kifu?$/.test(file);
 });
 var ok=0, ng=0, ngs=[];
 files.forEach(function(file){
 	var f = fs.readFileSync(dir+file);
-	var kifu = iconv.convert(f).toString();
+	var kifu = /.*\.kifu$/.test(file) ? f.toString().replace(/^\ufeff/, "") : iconv.convert(f).toString();
+	console.log(file, ",", kifu.charCodeAt(0));
 	try{
 		JKFPlayer.parseKIF(kifu);
 		ok++;
