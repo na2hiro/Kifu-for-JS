23cba3a1167a08078a1ea80be42cba3ac45d0e31
na2hiro <a@81.la>
2018-03-17 23:14:23 +0900

    Build for browsers

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 const path = require('path');
-const glob = require('glob');
+const webpack = require('webpack');
+const merge = require('webpack-merge');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const package = require('./package.json');
 
-const DIST_DIR = path.resolve(__dirname, './dist');
-
-module.exports = {
+const common = {
     entry: {
         filename: path.resolve(__dirname, './src/shogi.ts')
     },
@@ -20,6 +20,25 @@ module.exports = {
     resolve: {
         extensions: ['.ts', '.js']
     },
+};
+
+// For browsers
+const BUNDLE_DIR = path.resolve(__dirname, './bundle');
+const bundle = merge(common, {
+    output: {
+        library: "ShogiJS",
+        filename: `shogi-${package.version}.min.js`,
+        path: BUNDLE_DIR
+    },
+    plugins: [
+        new CleanWebpackPlugin([BUNDLE_DIR]),
+        new webpack.optimize.UglifyJsPlugin()
+    ]
+});
+
+// For npm module
+const DIST_DIR = path.resolve(__dirname, './dist');
+const dist = merge(common, {
     output: {
         libraryTarget: "commonjs2",
         filename: 'shogi.js',
@@ -28,4 +47,5 @@ module.exports = {
     plugins: [
         new CleanWebpackPlugin([DIST_DIR])
     ]
-};
+});
+module.exports = [bundle, dist];
