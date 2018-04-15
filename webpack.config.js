7d6e4b7a56ba714e807ca2eb24084ca55756e0c0
na2hiro <a@81.la>
2018-04-15 14:41:01 +0900

    Set up BundleAnalyzer

--- webpack.config.js
+++ webpack.config.js
@@ -2,50 +2,58 @@ const path = require('path');
 const webpack = require('webpack');
 const merge = require('webpack-merge');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const package = require('./package.json');
 
-const common = {
-    entry: {
-        filename: path.resolve(__dirname, './src/shogi.ts')
-    },
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: 'ts-loader',
-                exclude: /node_modules/
-            }
+module.exports = env => {
+    const IS_ANALYZE = env && env.analyze;
+    const common = {
+        entry: {
+            filename: path.resolve(__dirname, './src/shogi.ts')
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/
+                }
+            ]
+        },
+        resolve: {
+            extensions: ['.ts', '.js']
+        },
+    };
+
+    // For browsers
+    const BUNDLE_DIR = path.resolve(__dirname, './bundle');
+    const bundle = merge(common, {
+        output: {
+            library: "ShogiJS",
+            filename: `shogi-${package.version}.min.js`,
+            path: BUNDLE_DIR
+        },
+        plugins: [
+            new CleanWebpackPlugin([BUNDLE_DIR]),
+            new webpack.optimize.UglifyJsPlugin()
         ]
-    },
-    resolve: {
-        extensions: ['.ts', '.js']
-    },
+    });
+
+    if (IS_ANALYZE) {
+        bundle.plugins.push(new BundleAnalyzerPlugin());
+    }
+
+    // For npm module
+    const DIST_DIR = path.resolve(__dirname, './dist');
+    const dist = merge(common, {
+        output: {
+            libraryTarget: "commonjs2",
+            filename: 'shogi.js',
+            path: DIST_DIR
+        },
+        plugins: [
+            new CleanWebpackPlugin([DIST_DIR])
+        ]
+    });
+    return [bundle, dist];
 };
-
-// For browsers
-const BUNDLE_DIR = path.resolve(__dirname, './bundle');
-const bundle = merge(common, {
-    output: {
-        library: "ShogiJS",
-        filename: `shogi-${package.version}.min.js`,
-        path: BUNDLE_DIR
-    },
-    plugins: [
-        new CleanWebpackPlugin([BUNDLE_DIR]),
-        new webpack.optimize.UglifyJsPlugin()
-    ]
-});
-
-// For npm module
-const DIST_DIR = path.resolve(__dirname, './dist');
-const dist = merge(common, {
-    output: {
-        libraryTarget: "commonjs2",
-        filename: 'shogi.js',
-        path: DIST_DIR
-    },
-    plugins: [
-        new CleanWebpackPlugin([DIST_DIR])
-    ]
-});
-module.exports = [bundle, dist];
