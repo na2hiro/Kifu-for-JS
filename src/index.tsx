fdbae065815cd6b6766986c01cf84951c0fecc9b
na2hiro <a@81.la>
2020-06-24 23:02:03 +0900

    Expose mobx

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,13 @@
  * This software is released under the MIT License.
  * http://opensource.org/licenses/mit-license.php
  */
+import * as mobx from "mobx";
 import * as React from "react";
 import { render } from "react-dom";
 import Kifu from "./Kifu";
 import KifuStore from "./stores/KifuStore";
 import { onDomReady } from "./utils/util";
+export { mobx };
 
 export function loadString(kifu: string, id?: string): Promise<KifuStore> {
     return loadCommon(null, kifu, id);
