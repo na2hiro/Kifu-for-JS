d918956b948db3913e12f78537ce1389d0e19824
na2hiro <a@81.la>
2023-08-28 14:27:47 +0900

    Implement download

--- packages/Kifu-for-JS/src/lite/SettingsModal.tsx
+++ packages/Kifu-for-JS/src/lite/SettingsModal.tsx
@@ -61,54 +61,8 @@ export const SettingsModal: React.FC<Props> = ({ onClose, kifuStore }) => {
                     margin: "20px",
                 }}
             >
-                {!kifuStore.staticOptions && (
-                    <div
-                        onClick={() => {
-                            const newValue = (userSetting.hapticFeedback = !userSetting.hapticFeedback);
-                            if (newValue) {
-                                navigator.vibrate(1);
-                            }
-                        }}
-                    >
-                        {userSetting.hapticFeedback ? (
-                            <svg
-                                fill="currentColor"
-                                style={{ filter: "drop-shadow(2px 2px 2px rgba(0,0,0, 0.2))", opacity: 0.5 }}
-                                xmlns="http://www.w3.org/2000/svg"
-                                height={48}
-                                viewBox="0 -960 960 960"
-                                width={48}
-                            >
-                                <path d="M0-365v-230h60v230H0Zm120 88v-406h60v406h-60Zm780-88v-230h60v230h-60Zm-120 88v-406h60v406h-60ZM300-120q-24.75 0-42.375-17.625T240-180v-600q0-24.75 17.625-42.375T300-840h360q24.75 0 42.375 17.625T720-780v600q0 24.75-17.625 42.375T660-120H300Zm0-60h360v-600H300v600Zm0 0v-600 600Z" />
-                            </svg>
-                        ) : (
-                            <svg
-                                fill="currentColor"
-                                style={{ filter: "drop-shadow(2px 2px 2px rgba(0,0,0, 0.2))", opacity: 0.5 }}
-                                xmlns="http://www.w3.org/2000/svg"
-                                height={48}
-                                viewBox="0 -960 960 960"
-                                width={48}
-                            >
-                                <path d="M280-453h400v-60H280v60ZM480-80q-82 0-155-31.5t-127.5-86Q143-252 111.5-325T80-480q0-83 31.5-156t86-127Q252-817 325-848.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 82-31.5 155T763-197.5q-54 54.5-127 86T480-80Zm0-60q142 0 241-99.5T820-480q0-142-99-241t-241-99q-141 0-240.5 99T140-480q0 141 99.5 240.5T480-140Zm0-340Z" />
-                            </svg>
-                        )}
-                        <div>Haptic feedback?</div>
-                    </div>
-                )}
-                <div>
-                    <svg
-                        fill="currentColor"
-                        style={{ filter: "drop-shadow(2px 2px 2px rgba(0,0,0, 0.2))", opacity: 0.5 }}
-                        xmlns="http://www.w3.org/2000/svg"
-                        height={48}
-                        viewBox="0 -960 960 960"
-                        width={48}
-                    >
-                        <path d="M480-313 287-506l43-43 120 120v-371h60v371l120-120 43 43-193 193ZM220-160q-24 0-42-18t-18-42v-143h60v143h520v-143h60v143q0 24-18 42t-42 18H220Z" />
-                    </svg>
-                    Download Kifu
-                </div>
+                <HapticFeedback kifuStore={kifuStore} userSetting={userSetting} />
+                <Download filePath={kifuStore.filePath} />
             </div>
             <div style={{ alignSelf: "end", margin: "15px" }}>
                 <a
@@ -144,5 +98,81 @@ export const SettingsModal: React.FC<Props> = ({ onClose, kifuStore }) => {
         </div>
     );
 };
+
+function Download({ filePath }) {
+    const isAvailable = !!filePath;
+    return (
+        <div
+            role="button"
+            aria-disabled={!isAvailable}
+            tabIndex={0}
+            onClick={() =>
+                isAvailable
+                    ? open(filePath)
+                    : alert(
+                          "棋譜ファイル読み込み以外の方式のためダウンロードできません。（要望次第では今後対応予定です）",
+                      )
+            }
+        >
+            <svg
+                fill="currentColor"
+                style={{ filter: "drop-shadow(2px 2px 2px rgba(0,0,0, 0.2))", opacity: 0.5 }}
+                xmlns="http://www.w3.org/2000/svg"
+                height={48}
+                viewBox="0 -960 960 960"
+                width={48}
+            >
+                {isAvailable ? (
+                    <path d="M480-313 287-506l43-43 120 120v-371h60v371l120-120 43 43-193 193ZM220-160q-24 0-42-18t-18-42v-143h60v143h520v-143h60v143q0 24-18 42t-42 18H220Z" />
+                ) : (
+                    <path d="m813-61-99-99H220q-24 0-42-18t-18-42v-143h60v143h434L520-354l-40 41-193-193 41-40L61-813l43-43 752 752-43 43ZM606-439l-43-44 67-66 43 43-67 67Zm-96-97-60-60v-204h60v264Zm290 294-60-60v-61h60v121Z" />
+                )}
+            </svg>
+            Download Kifu
+        </div>
+    );
+}
+
+function HapticFeedback({ kifuStore, userSetting }) {
+    return (
+        !kifuStore.staticOptions && (
+            <div
+                role="button"
+                tabIndex={0}
+                onClick={() => {
+                    const newValue = (userSetting.hapticFeedback = !userSetting.hapticFeedback);
+                    if (newValue) {
+                        navigator.vibrate(1);
+                    }
+                }}
+            >
+                {userSetting.hapticFeedback ? (
+                    <svg
+                        fill="currentColor"
+                        style={{ filter: "drop-shadow(2px 2px 2px rgba(0,0,0, 0.2))", opacity: 0.5 }}
+                        xmlns="http://www.w3.org/2000/svg"
+                        height={48}
+                        viewBox="0 -960 960 960"
+                        width={48}
+                    >
+                        <path d="M0-365v-230h60v230H0Zm120 88v-406h60v406h-60Zm780-88v-230h60v230h-60Zm-120 88v-406h60v406h-60ZM300-120q-24.75 0-42.375-17.625T240-180v-600q0-24.75 17.625-42.375T300-840h360q24.75 0 42.375 17.625T720-780v600q0 24.75-17.625 42.375T660-120H300Zm0-60h360v-600H300v600Zm0 0v-600 600Z" />
+                    </svg>
+                ) : (
+                    <svg
+                        fill="currentColor"
+                        style={{ filter: "drop-shadow(2px 2px 2px rgba(0,0,0, 0.2))", opacity: 0.5 }}
+                        xmlns="http://www.w3.org/2000/svg"
+                        height={48}
+                        viewBox="0 -960 960 960"
+                        width={48}
+                    >
+                        <path d="M280-453h400v-60H280v60ZM480-80q-82 0-155-31.5t-127.5-86Q143-252 111.5-325T80-480q0-83 31.5-156t86-127Q252-817 325-848.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 82-31.5 155T763-197.5q-54 54.5-127 86T480-80Zm0-60q142 0 241-99.5T820-480q0-142-99-241t-241-99q-141 0-240.5 99T140-480q0 141 99.5 240.5T480-140Zm0-340Z" />
+                    </svg>
+                )}
+                <div>Haptic feedback?</div>
+            </div>
+        )
+    );
+}
 
 export default observer(SettingsModal);
